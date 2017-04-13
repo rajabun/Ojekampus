cd3ccc8026ca30cc4d166436d3f4191365ff9a07
rajabun <ichijo_rajab@hotmail.com>
2017-04-13 10:23:40 +0700

    Tugas 1 2 dan 3 Modul 7

---
+++ PesananOlehPelangganDitemukanException.java
@@ -0,0 +1,43 @@
+/**
+ * Write a description of class PesananOlehPelangganDitemukanException here.
+ * 
+ * @author Muhammad Rajab(1206244415)
+ * @version 0.7, 13 April 2017
+ * @version 0.6, 30 Maret 2017
+ * @version 0.5, 23 Maret 2017 
+ * @version 0.4, 19 Maret 2017
+ * @version 0.3, 18 Maret 2017
+ * @version 0.2, 02 Maret 2017 
+ * @since 0.1, 23 Februari 2017
+ */
+
+/**
+ * Class PesananOlehPelangganDitemukanException ditambahkan pada versi 0.7 (Modul 7)
+ */
+public class PesananOlehPelangganDitemukanException extends Exception
+{
+    // instance variables - replace the example below with your own
+    private Pelanggan pelanggan_error;
+
+    /**
+     * Constructor for objects of class PesananOlehPelangganException
+     */
+    public PesananOlehPelangganDitemukanException(Pelanggan pelanggan_input)
+    {
+        // initialise instance variables
+        super("Pesanan oleh");
+        pelanggan_input = pelanggan_error;
+    }
+
+    /**
+     * Method untuk menampilkan pesan exception
+     * 
+     * @return println Mengembalikan isi data dari println dalam bentuk string
+     */
+    @Override
+    public String getMessage()
+    {
+        return super.getMessage() + pelanggan_error.getNama() + " tidak ditemukan";
+    }
+    
+}
---
+++ PesananTidakDitemukanException.java
@@ -0,0 +1,44 @@
+
+/**
+ * Write a description of class PesananTidakDitemukanException here.
+ * 
+ * @author Muhammad Rajab(1206244415)
+ * @version 0.7, 13 April 2017
+ * @version 0.6, 30 Maret 2017
+ * @version 0.5, 23 Maret 2017 
+ * @version 0.4, 19 Maret 2017
+ * @version 0.3, 18 Maret 2017
+ * @version 0.2, 02 Maret 2017 
+ * @since 0.1, 23 Februari 2017
+ */
+
+/**
+ * Class PesananTidakDitemukanException ditambahkan pada versi 0.7 (Modul 7)
+ */
+public class PesananTidakDitemukanException extends Exception
+{
+    // instance variables - replace the example below with your own
+    private Pesanan pesanan_error;
+
+    /**
+     * Constructor for objects of class PesananTidakDitemukanException
+     */
+    public PesananTidakDitemukanException(Pesanan pesanan_input)
+    {
+        // initialise instance variables
+        super("Pesanan yang dipesan oleh");
+        pesanan_input = pesanan_error;
+    }
+
+    /**
+     * Method untuk menampilkan pesan exception
+     * 
+     * @return println Mengembalikan isi data dari println dalam bentuk string 
+     */
+    @Override
+    public String getMessage()
+    {
+        return super.getMessage() + pesanan_error.getPelanggan().getNama() + " tidak ditemukan";
+    }
+    
+}
